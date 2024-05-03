77386d6f8d24b0544aad69f56df8f3ddf11efed9
DESKTOP-K61H63V\kimyounghyun <kimyh9797@naver.com>
2024-05-03 09:54:05 +0900

    [Fix] 하단 수정

--- src/components/CuFooter.js
+++ src/components/CuFooter.js
@@ -11,18 +11,6 @@ const CuFooter = () => {
         textAlign: "left",
       }}
     >
-      <span>ABOUT US</span>
-      <div />
-      <span>WOORI ARTE는 누구나 작가가 될 수 있고 어쩌구 신념에서 탄생함</span>
-
-      <div
-        style={{
-          width: "100%",
-          height: 1,
-          backgroundColor: "white",
-          marginTop: 40,
-        }}
-      ></div>
       <span style={{ fontSize: 10, color: grey }}>
         서울 마포구 월드컵북로 434 상암 IT Tower
         <div />© 2024 WOORI ARTE. All Rights Reserved. Design by WOORI FISA
