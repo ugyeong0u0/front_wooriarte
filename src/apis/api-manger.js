f36b170d04ece7a10e36fe81a2546798b76b97a3
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-05-07 10:55:55 +0900

    Update api-manger.js

--- src/apis/api-manger.js
+++ src/apis/api-manger.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jsx } from "react/jsx-runtime";
 // 변수명 임시 지정
-const DOMAIN = "https://www.wooriarte.store/api"; // TODO: 도메인 주소 확인 필요
+const DOMAIN = "https://www.wooriarte.store/api"; // TODO: 도메인 주소 확인 필요!!!!
 
 //?----------------------------- 유저 url
 const LoginSpace_URL = () => `${DOMAIN}/users/login`; //! 로그인 수정완
