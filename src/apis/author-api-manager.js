92e5f4e7f2d628154518696752c6e7f6d297c6e0
Jin Minho <105831006+mlnho@users.noreply.github.com>
2024-05-07 10:37:58 +0900

    Update author-api-manager.js

--- src/apis/author-api-manager.js
+++ src/apis/author-api-manager.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { jsx } from "react/jsx-runtime";
 // 변수명 임시 지정
-const DOMAIN = "http://localhost:8080/api"; // TODO: 도메인 주소 확인 필요
+const DOMAIN = "https://www.wooriarte.store/api"; // TODO: 도메인 주소 확인 필요
 
 //?----------------------------- 작가 url
 // 로그인
